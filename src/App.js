454e802dbfdce6156d5b5172078dd558526c4f2e
Omolwethu <137441963+Omolwethu@users.noreply.github.com>
2024-04-16 00:52:32 +0200

    Included GitHub link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1> Weather App</h1>
+      <footer>
+        This project was created by{" "}
+        <a
+          href="https://heartfelt-gumdrop-809c5e.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Olwethu Zulu
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Omolwethu/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
