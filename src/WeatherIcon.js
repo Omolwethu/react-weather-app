6a18cdd1a116d984d662de4d7ea526c98e3a71ac
Omolwethu <137441963+Omolwethu@users.noreply.github.com>
2024-04-20 01:27:02 +0200

    Updated CSS.

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#9ba6a5"
-      size={64}
+      size={74}
       animate={true}
     />
   );
