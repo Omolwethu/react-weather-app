4f0625830cdea5cf6138377fece3d205695cf227
Omolwethu <137441963+Omolwethu@users.noreply.github.com>
2024-04-20 02:29:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather App</h1>
         <Weather defaultCity="Tokyo" />
         <footer>
           This project was created by{" "}
